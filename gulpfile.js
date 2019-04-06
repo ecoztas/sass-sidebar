5cc5af4f8af476e89a9903c5257f4a215bcc3b58
Emre Can ÖZTAŞ <me@emrecanoztas.com>
2019-04-06 04:00:45 +0300

    Ekleme

---
+++ gulpfile.js
@@ -0,0 +1,36 @@
+/**
+ * @name            sass-sidebar
+ * @file            gulpfile.js
+ * @version         1.0.0
+ * @description     Creating Sidebar With SASS
+ * @repository      https://github.com/oztasemrecan/sass-sidebar
+ * @author          Emre Can ÖZTAŞ (ecoztas) <me@emrecanoztas.com>
+ * @license         MIT
+ */
+
+// DEFINE: Package(s).
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var cssmin = require('gulp-cssmin');
+var plumber = require('gulp-plumber');
+var rename = require('gulp-rename');
+
+// TASK: Default
+gulp.task('default', function() {
+    console.log('Works fine!');
+});
+
+// TASK: LESS
+gulp.task('sass', function() {
+    return (
+        gulp.src('./sass/*.scss')
+        .pipe(plumber())
+        .pipe(sass())
+        .pipe(gulp.dest('dist'))
+        .pipe(cssmin())
+        .pipe(rename({
+            suffix: '.min'
+        }))
+        .pipe(gulp.dest('dist'))
+    );
+});
